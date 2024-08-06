40b08047c9326cbee765d264554dfcb744904696
Igor Miotto <igorenrique8810@gmail.com>
2024-08-06 17:36:25 -0300

    Update cadastrar.js

--- Unidade1/modulo6/RevisãoProva/cadastrar.js
+++ Unidade1/modulo6/RevisãoProva/cadastrar.js
@@ -1,10 +1,5 @@
-function cadastrarResidencias(callback) {
-    let bairro  = prompt('Qual o Bairro do Morador: ')
-    let rua = prompt('Qual a Rua do Morador: ')
-    let numero = prompt('Qual o Numero da Casa do Morador: ')
-    let morador = prompt('Qual o Nome do Morador: ')
-    residencias.push({bairro, rua, numero, morador})
-    console.log('Morador Cadastrado')
-    callback()
+function cadastrarResidencias(residencia) {
+    let id = residencia[residencias.length - 1].id + 1
+    residencias.push(residencia)
 }
 module.exports = cadastrarResidencias
