1ace381039e36e36ceffeb65ec73e472261efc93
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:30:13 -0300

    Update menuFlash.js

--- Unidade2/modulo7/ID4/flashcards/menuFlash.js
+++ Unidade2/modulo7/ID4/flashcards/menuFlash.js
@@ -1,8 +1,8 @@
-const adicionarFlashcards = require('./adiciona')
-const listarFlashcards = require('./lista')
-const editarFlashcards = require('./edita')  
-const removerFlashcards = require('./remove')
-const Flashcards = require('./Flashcards')
+const adicionarFlashcard = require('./adicionarFlashcards');
+const listarFlashcards = require('./listarFlashcards');
+const editarFlashcard = require('./editarFlashcards');
+const removerFlashcard = require('./removerFlashcards');
+const Flashcards = require('./Flashcards');
 
 function Menu() {
     console.log(`
@@ -10,71 +10,74 @@ function Menu() {
     1. Adicionar Flashcard
     2. Listar Todos os Flashcards
     3. Listar Flashcards por Baralho
-    4. Fazer uma busca por Baralho
-    5. Fazer uma busca por Pergunta
+    4. Fazer uma Busca por Baralho
+    5. Fazer uma Busca por Pergunta
     6. Editar Flashcard
     7. Remover Flashcard
     8. Voltar para o Menu Principal
-    `)
-    let opçao = prompt('Qual a sua Escolha: ')
-    let index
-    switch (opçao) {
+    `);
+
+    let opcao = prompt('Qual a sua Escolha: ');
+    let index, posicao;
+
+    switch (opcao) {
         case '1':
             const pergunta = prompt('Pergunta: ');
             const resposta = prompt('Resposta: ');
-            let Id = Flashcards[Flashcards.length - 1].Id + 1
-            adicionarFlashcards({ id: Id, Pergunta: pergunta, resposta: resposta});
+            let id = Flashcards.length > 0 ? Flashcards[Flashcards.length - 1].Id + 1 : 1;
+            adicionarFlashcard({ Id: id, pergunta: pergunta, resposta: resposta });
             console.log('FlashCard adicionado com sucesso!');
-            Menu()
+            Menu();
             break;
         case '2':
-            listarFlashcards()
-            Menu()
+            listarFlashcards();
+            Menu();
             break;
         case '3':
+            // Implementar a funcionalidade para listar flashcards por baralho
             break;
         case '4':
+            // Implementar a funcionalidade para busca por baralho
             break;
         case '5':
+            // Implementar a funcionalidade para busca por pergunta
             break;
         case '6':
-            listarFlashcards()
-            index = parseInt(prompt('Digite o ID que Deseja Editar: ')) - 1
-            posicao = usuarios.findIndex(usuarios => usuarios.Id == index)
-            if (posicao < 0 || posicao >= usuarios.length) {
-                console.log('Id não Encontrado, ou não Existente')
-                exibirMenu()
+            listarFlashcards();
+            index = parseInt(prompt('Digite o ID que Deseja Editar: ')) - 1;
+            posicao = Flashcards.findIndex(flashcard => flashcard.Id === index + 1);
+            if (posicao < 0) {
+                console.log('ID não Encontrado, ou não Existente');
             } else {
-                const novaPergunta = prompt('Pergunta: ');
-                const novaResposta = prompt('Resposta: ')
-                editarFlashcards( index, {id: baralhos[index].Id, Pergunta: novaPergunta, resposta: novaResposta})
-                console.log('FlashCard Editado com Sucesso')
+                const novaPergunta = prompt('Nova Pergunta: ');
+                const novaResposta = prompt('Nova Resposta: ');
+                editarFlashcard(posicao, novaPergunta, novaResposta);
+                console.log('FlashCard Editado com Sucesso');
             }
-            Menu()
+            Menu();
             break;
         case '7':
-            listarFlashcards()
+            listarFlashcards();
             index = parseInt(prompt('Digite o ID que Deseja Remover: ')) - 1;
-            posicao = baralhos.findIndex(baralhos => baralhos.Id == index)
-            if (posicao < 0 || posicao >= baralhos.length) {
-                console.log('Id não Encontrado, ou não Existente')
-                exibirMenu()
+            posicao = Flashcards.findIndex(flashcard => flashcard.Id === index + 1);
+            if (posicao < 0) {
+                console.log('ID não Encontrado, ou não Existente');
             } else {
-                removerFlashcards(FlashCards, posicao)
+                removerFlashcard(posicao);
+                console.log('FlashCard removido com sucesso!');
             }
-            console.log('FlashCard removido com sucesso!');
             Menu();
             break;
         case '8':
-            exibirMenu()
+            // Voltar para o menu principal
             break;
         default:
-            console.log('Opção Invalida, Por Favor, Tente Novamente')
-            Menu()
+            console.log('Opção Inválida, Por Favor, Tente Novamente');
+            Menu();
             break;
     }
 }
 
-Menu()
+Menu();
 
-module.exports = Menu
+module.exports = Menu;
