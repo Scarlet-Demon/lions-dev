520faab70f913b9e56d09f8dbe1243d7bce79cee
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:18:31 -0300

    Update listar.js

--- Unidade2/modulo7/ID4/baralho/listar.js
+++ Unidade2/modulo7/ID4/baralho/listar.js
@@ -1,12 +1,13 @@
-const baralhos = require("./baralhos");
+const baralhos = require('./baralhos');
 
 function listarBaralho() {
     for (let i = 0; i < baralhos.length; i++) {
         const baralho = baralhos[i];
         console.log(`
-        Id: ${baralho.id},
-        Titúlo: ${baralho.titulo},
+        Id: ${baralho.id}
+        Título: ${baralho.titulo}
         `);
     }
 }
-module.exports = listarBaralho
+
+module.exports = listarBaralho;
