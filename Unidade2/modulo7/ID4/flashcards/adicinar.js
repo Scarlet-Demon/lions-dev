0a866976564f01ac507f19dc87cc6b8f976b296e
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:31:46 -0300

    Update adicinar.js

--- Unidade2/modulo7/ID4/flashcards/adicinar.js
+++ Unidade2/modulo7/ID4/flashcards/adicinar.js
@@ -1,7 +1,8 @@
-const Flashcards = require('./Flashcards')
+const Flashcards = require('./Flashcards');
 
-function adicionarFlashcards(Flashcard) {
-    Flashcard.Id = Flashcards.length + 1
-    Flashcards.push(Flashcard)
+function adicionarFlashcard(flashcard) {
+    flashcard.Id = Flashcards.length > 0 ? Flashcards[Flashcards.length - 1].Id + 1 : 1;
+    Flashcards.push(flashcard);
 }
-module.exports = adicionarFlashcards
+
+module.exports = adicionarFlashcard;
