24092e6b2eb80ffb2f1e83f462580701b801d389
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:29:45 -0300

    Update remover.js

--- Unidade2/modulo7/ID4/flashcards/remover.js
+++ Unidade2/modulo7/ID4/flashcards/remover.js
@@ -1,1 +1,11 @@
 const Flashcards = require('./Flashcards')
+
+function removerFlashcards(index) {
+    if (index >= 0 && index < Flashcards.length) {
+        Flashcards.splice(index, 1);
+    } else {
+        console.log('Índice não encontrado');
+    }
+}
+
+module.exports = removerFlashcards;
