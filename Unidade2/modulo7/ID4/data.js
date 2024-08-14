78be7ac307b12cb0033e87512fe4a3a8fae027e6
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:16:54 -0300

    Update data.js

--- Unidade2/modulo7/ID4/data.js
+++ Unidade2/modulo7/ID4/data.js
@@ -1,19 +1,19 @@
 let baralhos = [
-    {id: 1, titulo: Historia},
-    {id: 2, titulo: Filosofia},
-    {id: 3, titulo: Sociologia}
-]
+    {id: 1, titulo: 'Historia'},
+    {id: 2, titulo: 'Filosofia'},
+    {id: 3, titulo: 'Sociologia'}
+];
 
 let flashCards = [
     {id: 1, pergunta: 'Quem descobriu o Brasil?', resposta: 'Pedro Alvarez Cabral', idBaralho: 1},
-    {id: 2, pergunta: 'Quem disse a segunte Frase: Só sei que Nada Sei?', resposta: 'Sócrates', idBaralho: 2},
-    {id: 3, pergunta: 'Defina Ética:', resposta: 'é uma área da filosofia que busca problematizar as questões relativas aos costumes e à moral de uma sociedade, sem recorrer ao senso comum.', idBaralho: 2},
-    {id: 4, pergunta: 'Defina Moral:', resposta: 'É o conjunto de valores e regras que definem o que é certo ou errado, permitido ou proibido em uma sociedade. ', idBaralho: 3},
-    {id: 5, pergunta: 'O que é Sociologia?', resposta: 'A Sociologia é uma das ciências humanas que estuda a sociedade', idBaralho: 3},
-    {id: 6, pergunta: 'Quais paises faziam parte da Triplice Aliança e da Triplice Entente?', resposta: 'Alemanha, Itália e Império Austro-Húngaro/França, Inglaterra e Rússia', idBaralho: 1},
-    {id: 7, pergunta: 'O que é Filosofia', resposta: 'É a construção de conceitos que irão servir de base para diversas áreas do conhecimento. ', idBaralho: 2},
-    {id: 8, pergunta: 'Quem foi Émile Durkheim?', resposta: 'Foi o fundador da sociologia, pelo fato de ter sido o primeiro a criar um método sociológico que distinguiu a sociologia das demais ciências humanas.', idBaralho: 3},
-    {id: 9, pergunta: 'O que foi a Revolução Francesa', resposta: 'Revolução Francesa foi um período, entre 1789 e 1799, de intensa agitação política e social na França, que teve um impacto duradouro na história do país e, mais amplamente, em todo o continente europeu.', idBaralho: 1},
-]
+    {id: 2, pergunta: 'Quem disse a seguinte frase: "Só sei que nada sei"?', resposta: 'Sócrates', idBaralho: 2},
+    {id: 3, pergunta: 'Defina Ética:', resposta: 'É uma área da filosofia que busca problematizar as questões relativas aos costumes e à moral de uma sociedade, sem recorrer ao senso comum.', idBaralho: 2},
+    {id: 4, pergunta: 'Defina Moral:', resposta: 'É o conjunto de valores e regras que definem o que é certo ou errado, permitido ou proibido em uma sociedade.', idBaralho: 3},
+    {id: 5, pergunta: 'O que é Sociologia?', resposta: 'A Sociologia é uma das ciências humanas que estuda a sociedade.', idBaralho: 3},
+    {id: 6, pergunta: 'Quais países faziam parte da Tríplice Aliança e da Tríplice Entente?', resposta: 'Alemanha, Itália e Império Austro-Húngaro / França, Inglaterra e Rússia', idBaralho: 1},
+    {id: 7, pergunta: 'O que é Filosofia?', resposta: 'É a construção de conceitos que servirão de base para diversas áreas do conhecimento.', idBaralho: 2},
+    {id: 8, pergunta: 'Quem foi Émile Durkheim?', resposta: 'Foi o fundador da sociologia por ter sido o primeiro a criar um método sociológico que distinguiu a sociologia das demais ciências humanas.', idBaralho: 3},
+    {id: 9, pergunta: 'O que foi a Revolução Francesa?', resposta: 'A Revolução Francesa foi um período, entre 1789 e 1799, de intensa agitação política e social na França, que teve um impacto duradouro na história do país e, mais amplamente, em todo o continente europeu.', idBaralho: 1},
+];
 
-module.exports = {baralhos, flashCards}
+module.exports = {baralhos, flashCards};
