9704aa8ec390014bd3411e6294b4996d355afd95
Igor Miotto <igorenrique8810@gmail.com>
2024-08-14 11:40:52 -0300

    Update adicionar.js

--- Unidade2/modulo7/ID4/baralho/adicionar.js
+++ Unidade2/modulo7/ID4/baralho/adicionar.js
@@ -1,7 +1,7 @@
 const baralhos = require('./baralhos');
 
 function adicionarBaralho(baralho) {
-    // Verifica se o array baralhos está vazio para definir o primeiro ID como 1
+    
     baralho.id = baralhos.length > 0 ? baralhos[baralhos.length - 1].id + 1 : 1;
     baralhos.push(baralho);
 }
